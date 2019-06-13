ce08242a17b45db94385e7284e4283094338f976
gileadn <49280308+gileadn@users.noreply.github.com>
2019-06-13 20:49:31 +0300

    header and footer

--- pages/homePage/HomePage.js
+++ pages/homePage/HomePage.js
@@ -12,3 +12,7 @@ function carousel() {
   x[slideIndex-1].style.display = "block";
   setTimeout(carousel, 5000); // Change image every 2 seconds
 }
+
+function makeMoney() {
+  location.replace("../catalog/catalog.html")
+}
