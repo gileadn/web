6e9626ede4ec7ba6732bf0f797ecaf80b3628beb
gileadn <49280308+gileadn@users.noreply.github.com>
2019-06-24 20:59:52 +0300

    ex 7 button

--- EX_7/JS_EX.js
+++ EX_7/JS_EX.js
@@ -1,5 +1,28 @@
-function f(element) {
-  let d = document.createElement("div");
-  d.innerHTML("<buttom>Click here</buttom>");
-  document.appendChild(d);
+
+document.querySelector("input").addEventListener("keyup" , type);
+
+function type() {
+  let inp = document.querySelector("input").value;
+  let btn = document.querySelector("button");
+    if(inp.length > 0)
+      btn.style.display = "inline-block";
+    else
+      btn.style.display = "none";
+}
+
+function addItem() {
+  let inp = document.querySelector("input").value;
+  let ul = document.querySelector("ul");
+  let li = document.createElement("li");
+  li.innerText = inp;
+  ul.appendChild(li);
+  ul.style.position = "relative";
+  ul.style.left = "31%";
+  ul.style.marginTop = "24px";
+  updateComplete()
+}
+
+function updateComplete() {
+  let ul = document.querySelector("ul");
+  ul.childElementCount
 }
