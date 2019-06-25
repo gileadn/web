5504bfef0e8f5da21974b57850e5d1cddf8dc20c
gileadn <49280308+gileadn@users.noreply.github.com>
2019-06-25 09:15:59 +0300

    ex 7 done

--- EX_7/JS_EX.js
+++ EX_7/JS_EX.js
@@ -1,5 +1,6 @@
 
 document.querySelector("input").addEventListener("keyup" , type);
+let count = 0;
 
 function type() {
   let inp = document.querySelector("input").value;
@@ -14,15 +15,38 @@ function addItem() {
   let inp = document.querySelector("input").value;
   let ul = document.querySelector("ul");
   let li = document.createElement("li");
-  li.innerText = inp;
+  li.setAttribute("onclick", "checkli(this)");
+  li.innerHTML = "<input type=checkbox class='box'>" + inp;
+  if (inp.length > 0) {
+
   ul.appendChild(li);
-  ul.style.position = "relative";
-  ul.style.left = "31%";
-  ul.style.marginTop = "24px";
-  updateComplete()
+  resetInput();
+  updateComplete(0);
+  document.querySelector("input").focus();
+}
 }
 
-function updateComplete() {
-  let ul = document.querySelector("ul");
-  ul.childElementCount
+function updateComplete(value) {
+  count+=value;
+  let comp = document.querySelector(".items");
+  comp.innerText = "Completed: "+ count+" items";
+}
+
+function resetInput() {
+  document.querySelector("input").value = "";
+  document.querySelector("button").style.display = "none"
+}
+
+function checkli(obj) {
+  obj.style.textDecoration = "line-through";
+  obj.firstChild.checked = "checked";
+  obj.firstChild.disabled = true;
+  obj.removeAttribute('onclick');
+  obj.setAttribute('class', 'marked');
+  updateComplete(1);
+}
+
+function enter(e) {
+  if(e.keyCode == 13)
+    addItem()
 }
