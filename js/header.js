bc8af9cdfd084d3a2a28238fd5295064bbc57d7b
gileadn <49280308+gileadn@users.noreply.github.com>
2019-07-01 19:45:57 +0300

    JS

--- js/header.js
+++ js/header.js
@@ -1,4 +1,21 @@
+let dis = 0;
+document.querySelector(".login_icon").addEventListener("click",login);
+
 function login() {
   let x = document.querySelector("fieldset.login");
-  x.style.display= "inline-block";
+  if (dis === 0) {
+  x.style.display = "inline-block";
+  dis = 1;
+  }
+  else
+  {
+    x.style.display = "none";
+    dis = 0;
+  }
+}
+
+function loginc() {
+  let x = document.querySelector("fieldset.login");
+  x.style.display= "none";
+  dis = 0;
 }
